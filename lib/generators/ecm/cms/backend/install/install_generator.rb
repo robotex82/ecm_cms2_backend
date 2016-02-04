33da6af90799dc822b92adc4effeea39cd6e049c
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2016-02-04 16:38:26 +0100

    Fixed linked pages link. Generator adds engine to the backend.

--- lib/generators/ecm/cms/backend/install/install_generator.rb
+++ lib/generators/ecm/cms/backend/install/install_generator.rb
@@ -7,8 +7,20 @@ module Ecm
 
           source_root File.expand_path('../templates', __FILE__)
 
-          def generate_intializer
+          def generate_initializer
             copy_file 'initializer.rb', 'config/initializers/ecm_cms_backend.rb'
+          end
+
+          def add_to_itsf_backend
+            insert_into_itsf_backend_config(Ecm::Cms::Backend::Engine)
+          end
+
+          private
+
+          def insert_into_itsf_backend_config(engine_name)
+            inject_into_file 'config/initializers/001_itsf_backend.rb', after: "config.backend_engines = %w(\n" do
+              "    #{engine_name}\n"
+            end
           end
         end
       end
