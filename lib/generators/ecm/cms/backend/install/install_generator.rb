a9ca7b8c2462f5b625aa1cec664a700cccffefa6
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-10-31 19:38:34 +0100

    Install generator now adds routes. remove required mark from pathname in forms.

--- lib/generators/ecm/cms/backend/install/install_generator.rb
+++ lib/generators/ecm/cms/backend/install/install_generator.rb
@@ -3,12 +3,16 @@ module Ecm
     module Backend
       module Generators
         class InstallGenerator < Rails::Generators::Base
-          desc 'Generates the intializer'
+          desc 'Generates the initializer'
 
           source_root File.expand_path('../templates', __FILE__)
 
           def generate_initializer
             copy_file 'initializer.rb', 'config/initializers/ecm_cms_backend.rb'
+          end
+
+          def generate_routes
+            route File.read(File.join(File.expand_path('../templates', __FILE__), 'routes.source'))
           end
 
           def add_to_itsf_backend
