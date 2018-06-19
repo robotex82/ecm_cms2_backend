2a80a19225febfeb790c5377273bbe5bf7e666a8
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-06-19 21:27:34 +0200

    Fix routes for services.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Ecm::Cms::Backend::Engine.routes.draw do
-  resources :add_homepages_service, only: [:index] do
-    get  :invoke, on: :collection
-    post :call,   on: :collection
-  end
-
-  resources :import_partials_service, only: [:index] do
-    get  :invoke, on: :collection
-    post :call,   on: :collection
-  end
+  resources :add_homepages_service, only: [:create, :new]
+  resources :import_partials_service, only: [:create, :new]
 
   backend_resources :content_boxes
   backend_resources :navigations
